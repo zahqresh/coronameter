4101e84a554cad577579cc76dd0694043af3b042
zehqresh <muhammadhamza2965@gmail.com>
2020-03-25 13:17:23 -0700

    template engine added, AXIOS API, UI, New feautres added

---
+++ README.md
@@ -0,0 +1,23 @@
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+CoronaMeter is web app that gives you recent data on coronavirus by default it show's Pakistan's data but you can search globally
+	
+## Technologies
+Project is created with:
+* Express
+* novelCovid API
+* Body-parser
+* HBS templating engine
+* AXIOS
+	
+## Setup
+To run this project, install it locally by cloning the repo or download the zip file using git clone https://github.com/zehqresh/coronameter.git :
+```
+$ npm intall
+$ node app.js
+```
+
--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 const express  = require('express');
 var app = express();
-var fs = require('fs')
+var fs = require('fs');
+const covid = require('novelcovid');
+const axios = require('axios');
+var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+//fetch data
 
-app.use(express.static(__dirname+'/public/condition'));
+//setup view engine
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.use(express.static(__dirname+'/public'));
-app.get('/',(req,res)=>{res.render('index')});
-app.get('/condition', (req, res) => {
-    fs.readFile(__dirname + '/public/condition.html', 'utf8', (err, text) => {
-        res.send(text);
-    });
+//setup bodyparser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+app.get('/',(req,res)=>{
+	axios.get('https://corona.lmao.ninja/countries/pakistan')
+  .then(function (response) {
+    res.render('index',{
+    	data:response.data
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+});
+app.get('/world', (req, res) => {
+	res.render('global');
+});
+app.get('/prevent',(req,res)=>{
+	res.render('fatal');
+});
+app.get('/about',(req,res)=>{
+	res.render('about');
 });
 
-app.get('/preventation', (req, res) => {
-    fs.readFile(__dirname + '/public/preventation.html', 'utf8', (err, text) => {
-        res.send(text);
-    });
+//post request
+app.post('/world',(req,res)=>{
+	let country = req.body.search;
+	axios.get(`https://corona.lmao.ninja/countries/${country}`)
+  .then(function (response) {
+  	
+    res.render('global',{
+    	data:response.data
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 });
+
+
+
+
 
 app.listen(process.env.PORT || 5000);
