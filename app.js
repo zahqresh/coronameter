49fdd4acce4176fb719abaf6eb0f2fc3317d0ac2
zehqresh <muhammadhamza2965@gmail.com>
2020-03-24 21:40:58 -0700

    Ui update ready to deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express  = require('express');
+var app = express();
 
-
-app.use(express.static(__dirname+'public'));
+app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{res.render('index')});
 var port = process.env.PORT || 3000;
-app.listen(3000,()=> console.log(`app runing on ${port}`))
+app.listen(3000,()=>{ console.log(`app runing on ${port}`)})
