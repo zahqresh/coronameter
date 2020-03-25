f360a8f232d7e58f50fc5a366b7f7e0db35acfc0
zehqresh <muhammadhamza2965@gmail.com>
2020-03-24 21:34:45 -0700

    chart updated and data included

---
+++ app.js
@@ -0,0 +1,7 @@
+const express  = require('express');
+
+
+app.use(express.static(__dirname+'public'));
+app.get('/',(req,res)=>{res.render('index')});
+var port = process.env.PORT || 3000;
+app.listen(3000,()=> console.log(`app runing on ${port}`))
