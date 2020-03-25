bf18dfd417a0a156e4632b0755edb278135bcca7
zehqresh <muhammadhamza2965@gmail.com>
2020-03-24 21:48:05 -0700

    heroku port fixed

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var app = express();
 
 app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{res.render('index')});
-var port = process.env.PORT || 3000;
-app.listen(3000,()=>{ console.log(`app runing on ${port}`)})
+
+app.listen(process.env.PORT || 5000);
