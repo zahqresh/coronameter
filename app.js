b66770a1a57d985104ae3f8a24b68cb00aed4846
zehqresh <muhammadhamza2965@gmail.com>
2020-03-24 22:46:07 -0700

    new page added with data

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express  = require('express');
 var app = express();
+var fs = require('fs')
 
+app.use(express.static(__dirname+'/public/condition'));
 app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{res.render('index')});
+app.get('/condition', (req, res) => {
+    fs.readFile(__dirname + '/public/condition.html', 'utf8', (err, text) => {
+        res.send(text);
+    });
+});
 
 app.listen(process.env.PORT || 5000);
